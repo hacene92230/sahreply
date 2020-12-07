9dec47318e78431e81e0a7a2c492164604a0a462
hacene92230 <33603117+hacene92230@users.noreply.github.com>
2020-12-07 16:44:49 +0100

    ajout du form de prestation

---
+++ src/Entity/Prestation.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PrestationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PrestationRepository::class)
+ */
+class Prestation
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $nbheure;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $statut;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getNbheure(): ?int
+    {
+        return $this->nbheure;
+    }
+
+    public function setNbheure(int $nbheure): self
+    {
+        $this->nbheure = $nbheure;
+
+        return $this;
+    }
+
+    public function getStatut(): ?bool
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(bool $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}

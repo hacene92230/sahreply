c6754d6a0867251fdf4ccf93c5637482d5d6abd4
hacenedu92@live.fr <hacenedu92@live.fr>
2021-02-24 16:37:50 +0100

    finission de la demande

--- src/Form/DemandeTypes.php
+++ src/Form/DemandeTypes.php
@@ -7,6 +7,7 @@ use App\Entity\Demande;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class DemandeTypes extends AbstractType
@@ -20,8 +21,8 @@ class DemandeTypes extends AbstractType
                 'choice_label' => 'nom',
                 'multiple' => true
             ])
-            ->add('cv')
-            ->add('motivation');
+            ->add('cv', TextareaType::class, ['label' => "Saisir votre parcour de vie durant les 5 dernières années", "attr" => ['placeholder' => "Votre CV"]])
+            ->add('motivation', TextareaType::class, ["label" => "Rédiger en un text cours l'ensemble des éléments qui vous pousse à postuler chez nous, et pourquoi on devrais vous embaucher vous et pas un autre", "attr" => ['placeholder' => "Saisir votre motivation"]]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
